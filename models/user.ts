7613d8216a84e2f4d6fe7e6006ea8f0cb22ba99b
Vincent <Vincent.ahlin99@gmail.com>
2024-05-11 16:07:24 +0200

    Fix user-model

--- models/user.ts
+++ models/user.ts
@@ -1,16 +1,9 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "Please provide a username"],
-    maxlength: [60, "Name cannot be more than 60 characters"],
-  },
-  password: {
-    type: String,
-    required: [true, "Please provide a password"],
-    maxlength: [60, "Password's Name cannot be more than 60 characters"],
-  },
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  isAdmin: { type: Boolean, require: false },
 });
 
 const User = mongoose.models.User || mongoose.model("User", UserSchema);
