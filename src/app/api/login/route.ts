066f010a87fc61e7b5300944b0f79dc7e4b9a8c1
Vincent <Vincent.ahlin99@gmail.com>
2024-05-14 16:53:01 +0200

    No middleware in routes

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,80 +3,75 @@ import { NextRequest, NextResponse } from "next/server";
 import User from "../../../../models/user";
 import dbConnect from "../../../../util/dbConnect";
 
-export async function middleware(req: NextRequest) {
+export async function POST(req: NextRequest) {
   await dbConnect();
 
-  // Parsing JSON body from req
-  if (req.method === "POST") {
-    const body = await req.json();
-    const { username, password } = body;
+  const body = await req.json();
+  const { username, password } = body;
 
-    // 2: Check if username or password is missing and if, return 400
-    if (!username || !password) {
-      return new NextResponse(
-        JSON.stringify({ message: "Missing username or password" }),
-        {
-          status: 400,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    }
-
-    // 3: Find user with provided username
-    const user = await User.findOne({ username: username });
-
-    // 4: If no user is found return 401
-    if (!user) {
-      return new NextResponse(
-        JSON.stringify({ message: "Invalid username or password" }),
-        {
-          status: 401,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    }
-
-    // 6: Check if we can't verify hashed password and password
-    if (!(await bcrypt.compare(password, user.password))) {
-      return new NextResponse(
-        JSON.stringify({ message: "Invalid username or password" }),
-        {
-          status: 401,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    }
-
-    // 7: Exclude password and prepare user object
-    const { password: _, ...authenticatedUser } = user.toObject();
-
-    // 8: Construct the response and set the username cookie
-    const response = new NextResponse(
-      JSON.stringify({ message: "Welcome", user: authenticatedUser }),
+  // 2: Check if username or password is missing and if, return 400
+  if (!username || !password) {
+    return new NextResponse(
+      JSON.stringify({ message: "Missing username or password" }),
       {
-        status: 200,
+        status: 400,
         headers: {
           "Content-Type": "application/json",
         },
       }
     );
-
-    // Set cookie using NextResponse's cookie method
-    response.cookies.set("username", username, {
-      path: "/",
-      maxAge: 60 * 60 * 24 * 7, // 7 days
-      httpOnly: true, // Important for security
-      sameSite: "strict",
-    });
-
-    return response;
   }
 
-  return new NextResponse(null, { status: 405 });
+  // 3: Find user with provided username
+  const user = await User.findOne({ username: username });
+
+  // 4: If no user is found return 401
+  if (!user) {
+    return new NextResponse(
+      JSON.stringify({ message: "Invalid username or password" }),
+      {
+        status: 401,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  // 6: Check if we can't verify hashed password and password
+  if (!(await bcrypt.compare(password, user.password))) {
+    return new NextResponse(
+      JSON.stringify({ message: "Invalid username or password" }),
+      {
+        status: 401,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  // 7: Exclude password and prepare user object
+  const { password: _, ...authenticatedUser } = user.toObject();
+
+  // 8: Construct the response and set the username cookie
+  const response = new NextResponse(
+    JSON.stringify({ message: "Welcome", user: authenticatedUser }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  // Set cookie using NextResponse's cookie method
+  response.cookies.set("username", username, {
+    path: "/",
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    httpOnly: true, // Important for security
+    sameSite: "strict",
+  });
+
+  return response;
 }
--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -8,7 +8,7 @@ import dbConnect from "../../../../util/dbConnect";
 
 const pipelineAsync = promisify(pipeline);
 
-export async function middleware(req: NextRequest) {
+export async function POST(req: NextRequest) {
   await dbConnect();
 
   const userSessionCookie = req.cookies.get("username");
@@ -31,76 +31,70 @@ export async function middleware(req: NextRequest) {
     });
   }
 
-  if (req.method === "POST") {
-    const bb = busboy({ headers: Object.fromEntries(req.headers) });
-    const uploadPromise = new Promise((resolve, reject) => {
-      bb.on("file", async (name, file, info) => {
-        const { filename, mimeType } = info;
-        const bucket = getImageBucket();
-        const uploadStream = bucket.openUploadStream(filename, {
-          metadata: { contentType: mimeType },
-        });
-
-        try {
-          await pipelineAsync(file, uploadStream);
-          resolve(uploadStream.id.toString());
-        } catch (error) {
-          reject(error);
-        }
+  const bb = busboy({ headers: Object.fromEntries(req.headers) });
+  const uploadPromise = new Promise((resolve, reject) => {
+    bb.on("file", async (name, file, info) => {
+      const { filename, mimeType } = info;
+      const bucket = getImageBucket();
+      const uploadStream = bucket.openUploadStream(filename, {
+        metadata: { contentType: mimeType },
       });
 
-      bb.on("finish", () => {
-        resolve(null);
-      });
-
-      bb.on("error", (error) => {
+      try {
+        await pipelineAsync(file, uploadStream);
+        resolve(uploadStream.id.toString());
+      } catch (error) {
         reject(error);
-      });
+      }
     });
 
-    try {
-      const blob = await req.blob();
-      const bufferStream = new Readable({
-        read() {
-          this.push(blob.stream());
-          this.push(null);
-        },
-      });
+    bb.on("finish", () => {
+      resolve(null);
+    });
 
-      bufferStream.pipe(bb);
+    bb.on("error", (error) => {
+      reject(error);
+    });
+  });
 
-      const imageId = await uploadPromise;
-      if (!imageId) {
-        return new NextResponse(
-          JSON.stringify({ message: "No file uploaded" }),
-          {
-            status: 400,
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-      }
+  try {
+    const blob = await req.blob();
+    const bufferStream = new Readable({
+      read() {
+        this.push(blob.stream());
+        this.push(null);
+      },
+    });
 
-      return new NextResponse(JSON.stringify({ imageId }), {
-        status: 201,
+    bufferStream.pipe(bb);
+
+    const imageId = await uploadPromise;
+    if (!imageId) {
+      return new NextResponse(JSON.stringify({ message: "No file uploaded" }), {
+        status: 400,
         headers: {
           "Content-Type": "application/json",
         },
       });
-    } catch (error) {
-      console.error("Error uploading file:", error);
-      return new NextResponse(
-        JSON.stringify({ error: "Internal Server Error" }),
-        {
-          status: 500,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
     }
-  }
 
+    return new NextResponse(JSON.stringify({ imageId }), {
+      status: 201,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    return new NextResponse(
+      JSON.stringify({ error: "Internal Server Error" }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
   return NextResponse.next();
 }
